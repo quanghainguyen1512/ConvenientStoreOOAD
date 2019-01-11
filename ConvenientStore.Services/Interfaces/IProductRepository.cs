45c387bf3db316a44f2d8659194016b7b4effc8f
AnhPmcl2015 <phananh.pmcl2015@gmail.com>
2019-01-12 00:02:29 +0700

    Cập nhật repository

---
+++ ConvenientStore.Services/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using ConvenientStore.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvenientStore.Services.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<ProductDetail> GetProductDetails();
+
+        ProductDetail GetProductDetailById(int productDetailId);
+
+        ProductDetail GetProductdetailByBarCode(string barcode);
+
+        bool IsProductDetailExist(string barcode);
+    }
+}
---
+++ ConvenientStore.Services/Repositories/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using ConvenientStore.DAO;
+using ConvenientStore.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvenientStore.Services.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public ProductDetail GetProductdetailByBarCode(string barcode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductDetail GetProductDetailById(int productDetailId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductDetail> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsProductDetailExist(string barcode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
