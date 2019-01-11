2afbebc059f4ed5bf2c322abad60e6ef53a0ca3e
AnhPmcl2015 <phananh.pmcl2015@gmail.com>
2019-01-11 21:19:21 +0700

    commit to the anh

--- ConvenientStore/Form1.cs
+++ ConvenientStore/Form1.cs
@@ -16,5 +16,11 @@ namespace ConvenientStore
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LapHoaDonForm form = new LapHoaDonForm();
+            form.Show();
+        }
     }
 }
--- ConvenientStore/LapHoaDonForm.cs
+++ ConvenientStore/LapHoaDonForm.cs
@@ -16,41 +16,5 @@ namespace ConvenientStore
         {
             InitializeComponent();
         }
-
-        // Xử lý load thông tin khách hàng khi rời khỏi trường [Mã khách hàng]
-        private void leaveCustomerCodeField(object sender, EventArgs e)
-        {
-            if("".Equals(this.txtCustomerCode.Text.Trim()))
-            {
-                return;
-            }
-        }
-
-        // Xử lý load thông tin khách hàng khi nhấn enter trường [Mã khách hàng]
-        private void enterCustomerCodeField(object sender, KeyEventArgs e)
-        {
-            if(e.KeyCode == Keys.Enter)
-            {
-
-            }
-        }
-
-        // Xử lý load thông tin sản phẩm khi rời khỏi trường [Mã sản phẩm]
-        private void leaveProductCode(object sender, EventArgs e)
-        {
-            if ("".Equals(this.txtProductCode.Text.Trim()))
-            {
-                return;
-            }
-        }
-
-        // Xử lý load thông tin sản phẩm khi nhấn enter trường [Mã sản phẩm]
-        private void enterProductCode(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-
-            }
-        }
     }
 }
